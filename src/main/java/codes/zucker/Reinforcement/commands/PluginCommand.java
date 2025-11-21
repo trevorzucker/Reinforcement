f36b425698da6664bf405c011b7412375602444b
trevorzucker <vxcheese@gmail.com>
2025-11-21 01:04:57 -0600

    Remove Reinforcement folder

--- src/main/java/codes/zucker/Reinforcement/commands/PluginCommand.java
+++
@@ -1,8 +0,0 @@
-package codes.zucker.reinforcement.commands;
-
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
-public interface PluginCommand {
-  boolean execute(CommandSender sender, Command cmd, String label, String[] args);
-}
